ec00f2cb752860d87e64fdb71dcd1d68b2f407d9
srinivasit12 <srinivasit12@gmail.com>
2018-10-31 14:11:08 +0530

    31-oct-2018

---
+++ src/main/java/SampleProject3/Exampleproject1/SoftwareEngineer.java
@@ -0,0 +1,12 @@
+package SampleProject3.Exampleproject1;
+
+public class SoftwareEngineer {
+	private String projectName;
+
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+}
---
+++ src/main/java/SampleProject3/Exampleproject1/TeamLead.java
@@ -0,0 +1,12 @@
+package SampleProject3.Exampleproject1;
+
+public class TeamLead {
+	private int numberOfProjectsManaged;
+
+	public int getNumberOfProjectsManaged() {
+		return numberOfProjectsManaged;
+	}
+	public void setNumberOfProjectsManaged(int numberOfProjectsManaged) {
+		this.numberOfProjectsManaged = numberOfProjectsManaged;
+	}
+}
